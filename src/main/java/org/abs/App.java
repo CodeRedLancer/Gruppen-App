3db5d84b74ef6955953fcfc192fb9f21418c9140
Alexander Behling <alexander.behling@valtech.com>
2022-11-03 10:10:56 +0100

    removed classes

--- src/main/java/org/abs/App.java
+++ src/main/java/org/abs/App.java
@@ -1,7 +1,6 @@
 package org.abs;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
@@ -12,11 +11,6 @@ public class App extends Application {
 
     @Override
     public void start(final Stage primaryStage) {
-        final var customBorderPane = new CustomBorderPane();
-        final var scene = new Scene(customBorderPane.getBorderPane(), 640, 480);
-
-        primaryStage.setScene(scene);
-        primaryStage.show();
     }
 
     public static void main(final String[] args) {
