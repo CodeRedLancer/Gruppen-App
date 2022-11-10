5311b650aac55f3e269f553464eaba719ed4b143
Tatiana Mironova <tatiana.mironova@valtech.com>
2022-11-10 12:24:27 +0100

    Add Lombok to classes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,5 +2,6 @@ module org.openjfx {
     requires javafx.controls;
     requires spring.boot.autoconfigure;
     requires java.persistence;
+    requires lombok;
     exports org.abs.gruppenapp;
 }
--- src/main/java/org/abs/gruppenapp/Entitys/Course.java
+++ src/main/java/org/abs/gruppenapp/Entitys/Course.java
@@ -10,47 +10,26 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity(name = "Course")
 @Table(name = "course_table")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Course {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int courseId;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int courseId;
 
-    @OneToMany(fetch = FetchType.LAZY)
-    private Set<Integer> teacherId = new HashSet<>();
+  @OneToMany(fetch = FetchType.LAZY)
+  private Set<Integer> teacherId = new HashSet<>();
 
-    @Column(name = "name")
-    private String name;
-
-    public Course() {
-    }
-
-    public Course(int courseId, Set<Integer> teacherId, String name) {
-        this.courseId = courseId;
-        this.teacherId = teacherId;
-        this.name = name;
-    }
-
-    public int getCourseId() {
-        return courseId;
-    }
-
-    public Set<Integer> getTeacherId() {
-        return teacherId;
-    }
-
-    public void setTeacherId(Set<Integer> teacherId) {
-        this.teacherId = teacherId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+  @Column(name = "name")
+  private String name;
 }
