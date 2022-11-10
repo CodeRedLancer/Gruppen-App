2b8c9803a247e98ea66ae283a41970b1ecda6de8
Alexander Behling <alexander.behling@valtech.com>
2022-11-10 12:29:24 +0100

    added repositories

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,5 +2,9 @@ module org.openjfx {
     requires javafx.controls;
     requires spring.boot.autoconfigure;
     requires java.persistence;
+    requires spring.context;
+    requires spring.data.commons;
+    requires spring.data.jpa;
     exports org.abs.gruppenapp;
+    exports org.abs.gruppenapp.repository;
 }
