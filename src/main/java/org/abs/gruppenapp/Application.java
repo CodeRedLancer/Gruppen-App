7a15ce959b962656758c321ac91e212dec4cd19e
Tatiana Mironova <tatiana.mironova@valtech.com>
2022-12-01 10:01:52 +0100

    Delete JavaFX and implement Swing with example GUI

--- src/main/java/org/abs/gruppenapp/Application.java
+++ src/main/java/org/abs/gruppenapp/Application.java
@@ -1,11 +1,11 @@
 package org.abs.gruppenapp;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.abs.gruppenapp.gui.SwingApp;
+import org.springframework.boot.SpringApplication;
 
-@SpringBootApplication
 public class Application {
 
   public static void main(String[] args) {
-    javafx.application.Application.launch(App.class, args);
+    SpringApplication.run(SwingApp.class, args);
   }
 }
